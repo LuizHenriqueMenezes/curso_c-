21dd3d285dbec1c13bc1d59abcff3e5d42718fc0
Luiz H. Menezes <79934587+LuizHenriqueMenezes@users.noreply.github.com>
2024-07-24 11:33:32 -0300

    enumerador

---
+++ aulas/aula10/Program.cs
@@ -0,0 +1,23 @@
+﻿// Enumeradores 
+
+using System; 
+
+namespace Aula10
+{
+    class Program
+    {
+        // dentro são os valores possíveis 
+        enum DiasSemana{Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado}; 
+        static void Main(string[] args)
+        {
+            // ele vai receber um valor apenas que for possível 
+            DiasSemana ds = DiasSemana.Domingo;
+
+            // pega o indice e manda pra uma váriavel int
+            int x = (int)DiasSemana.Sexta;
+
+            Console.WriteLine(ds);
+            Console.WriteLine(x);
+        }
+    }
+}
