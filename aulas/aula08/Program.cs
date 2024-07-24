a66dd339a531c5908d2a72051165d212e31e352b
Luiz H. Menezes <79934587+LuizHenriqueMenezes@users.noreply.github.com>
2024-07-24 11:15:51 -0300

    lendo valores no teclado e bitwise

---
+++ aulas/aula08/Program.cs
@@ -0,0 +1,31 @@
+﻿// lendo valores no teclado
+
+using System; 
+
+namespace Aula08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v1, v2, soma; 
+            string nome; 
+
+            Console.Write("\nDigite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write(nome);
+
+            Console.WriteLine("\n---------------------");
+            Console.Write("Digite um valor: ");
+            v1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite mais um valor: ");
+            v2 = Convert.ToInt32(Console.ReadLine());
+
+            soma = v1 + v2; 
+
+            Console.WriteLine($"\nA soma de {v1} + {v2} é {soma}");
+        }
+    }
+}
---
+++ aulas/aula09/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Operações de Bitwise
+<< Dobrar
+>> Metade 
+exemplo 10 = 00001010, se dobrar (>>):
+00010100 = 20, vai passando uma cada pra esquerda pra dobrar, 
+e pra direita pra dividir 
+
+é possível fazer bitwise de números além de 1, 
+por exemplo 2, que muda 2 casas pra esqueda ou direita
+*/
+
+using System; 
+
+namespace Aula09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10; 
+
+            num = num << 1; 
+
+            Console.WriteLine(num);
+        }
+    }
+}
