e7e2118d0f71d2cd736ea442b206ba0742f8fdca
Luiz H. Menezes <79934587+LuizHenriqueMenezes@users.noreply.github.com>
2024-07-25 11:12:00 -0300

    type cast

--- aulas/aula03/obj/Debug/net8.0/aula03.AssemblyInfo.cs
+++ aulas/aula03/obj/Debug/net8.0/aula03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f13fce3f8e888b97da10cbf125ee3e3d8da34843")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21dd3d285dbec1c13bc1d59abcff3e5d42718fc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ aulas/aula11/Program.cs
@@ -0,0 +1,35 @@
+﻿// Conversões de tipos
+
+using System; 
+
+namespace Aula11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // algumas conversões ja são implicitas
+            int n1 = 10; 
+            float n2 = n1; 
+
+            Console.WriteLine(n2);
+
+            /* 
+            não implicita alerta que não pode converter
+            da erro:
+            float x = 10.5;
+            int y = x; 
+
+            Console.WriteLine(y);
+            */
+
+            // fazendo a conversão type cast:
+
+            float x = 10.5f; // quando é float precisa colocar o sufixo f
+            int y = (int)x; // eliminou o 0.5 
+
+            Console.WriteLine(y);
+            
+        }
+    }
+}
